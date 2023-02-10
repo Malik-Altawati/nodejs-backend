5e0196978e918b6a89edcd4f9d26823b06ddca1c
Malik Altawati <mariambarghathi5@gmail.com>
2023-02-10 16:40:44 +0200

    auth complete

--- src/domains/forgot_password/routes.js
+++ src/domains/forgot_password/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { sendPasswordResetOTPEmail} = require("./controller");
+const { sendPasswordResetOTPEmail,resetUserPassword} = require("./controller");
 
 //  Password reset request
 router.post("/", async(req,res) =>{
@@ -10,6 +10,19 @@ router.post("/", async(req,res) =>{
 
        const createdPasswordResetOTP = await sendPasswordResetOTPEmail(email);
         res.status(200).json(createdPasswordResetOTP);
+    } catch (error){
+        res.status(400).send(error.message);
+    }
+});
+
+//  reset
+router.post("/reset", async(req,res) =>{
+    try{
+        let {email, otp, newPassword} = req.body;
+       if(!(email && otp && newPassword)) throw Error("Empty creds are not allowed");
+        await resetUserPassword({email,otp,newPassword});
+
+        res.status(200).json({email,passwordReset:true});
     } catch (error){
         res.status(400).send(error.message);
     }
